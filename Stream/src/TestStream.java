1fa974e71f8d33fdfbe89d7db77bdcb73623258a
rodolfoSantosUK <santoseuro2018@gmail.com>
2020-09-23 14:04:10 -0300

    Uso de Streams

--- Stream/src/TestStream.java
+++ Stream/src/TestStream.java
@@ -15,24 +15,55 @@ public class TestStream {
         estudantes.add("Thiago");
         estudantes.add("Rafael");
 
-        System.out.println("Quantidade " +  estudantes.stream().count());
+        System.out.println("Quantidade " + estudantes.stream().count());
+
         System.out.println("Maior numero de letras " + estudantes
                 .stream()
                 .max(Comparator.comparing(String::length)));
 
         System.out.println("Menor numero de letras " + estudantes
-                                                       .stream()
-                                                       .min(Comparator.comparing(String::length)));
+                .stream()
+                .min(Comparator.comparing(String::length)));
 
         System.out.println(" Elemento com letra r " + estudantes
-        .stream()
-        .filter((estudante) ->
-                estudante.toLowerCase().contains("r")).collect(Collectors.toList()));
+                .stream()
+                .filter((estudante) ->
+                        estudante.toLowerCase().contains("r")).collect(Collectors.toList()));
 
-        System.out.println(" Nome mais a quantidade de letras "  + estudantes
-                .stream().map( estudante ->
+        System.out.println(" Nome mais a quantidade de letras " + estudantes
+                .stream().map(estudante ->
                         estudante.concat(" - ")
-                .concat(String.valueOf(estudante.length()))).collect(Collectors.toList()) );
+                                 .concat(String.valueOf(estudante.length()))).collect(Collectors.toList()));
+
+        System.out.println(" Retorna os 3 primeiros elementos: " + estudantes
+                .stream()
+                .limit(3).collect(Collectors.toList()));
+
+//      Exibe cada elemento no console e depois retorna a mesma coleção
+        System.out.println(" Retorna os elementos: " + estudantes
+                .stream()
+                .peek(System.out::println)
+                .collect(Collectors.toList()));
+
+        // Exibe cada elemento
+        System.out.println(" Retorna os elementos novamente: ");
+        estudantes.stream().forEach(System.out::println);
+
+        // Retorna true se todos os elements possuirem letra w no nome
+        System.out.println(" tem algum elemento com a letra w no nome ? " +
+                estudantes
+                        .stream()
+                        .allMatch((elemento) -> elemento.contains("w")));
+
+        //  Retorna true se algum dos elementos
+        System.out.println(" Possui algum elemento com a ?  " +
+                estudantes
+                        .stream()
+                        .anyMatch((estudante) ->
+                                estudante.toLowerCase().contains("a"))
+        );
+
+
 
     }
 
